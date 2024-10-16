67310222cf92a64d027f35735ab4acbbdcf7feb2
Gaurav Tiwari <gauravtiwari01@gmail.com>
2024-10-15 23:57:22 -0400

    Concurrent Hashmap WIP

--- concurrent_collections/core.py
+++ concurrent_collections/core.py
@@ -1,20 +1,24 @@
 import hashlib
 import loguru
+from threading import RLock
 
 class ConcurrentHashMap:
     def __init__(self, capacity=20) -> None:
-        # each bucket is a dictionary 
+        # each bucket is a dictionary and has an associated lock
         self.capacity = capacity
         self.buckets = [dict() for _ in range(capacity)]
+        self.locks = [RLock() for _ in range(capacity)]
 
     def put(self, key, value):
         bucket_num = self.hash(key) % self.capacity
-        self.buckets[bucket_num][key] = value
-        return value
+        with self.locks[bucket_num]:
+            self.buckets[bucket_num][key] = value
+            return value
         
     def get(self, key):
         bucket_num = self.hash(key) % self.capacity
-        return self.buckets[bucket_num][key]
+        with self.locks[bucket_num]:
+            return self.buckets[bucket_num][key]
 
     def containsKey(self, key) -> bool:
         bucket_num = self.hash(key) % self.capacity
